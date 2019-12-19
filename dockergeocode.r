c4a7ae37f5dc808ba04cbd1672afd6288750afce
Dieter Castel <dietercastel@gmail.com>
2019-12-19 19:48:31 +0100

    Added street data and retrieval R script.

---
+++ dockergeocode.r
@@ -0,0 +1,210 @@
+library(sf)
+library(osmdata)
+library(tmap)
+library(tmaptools)
+# required for map
+library(purrr)
+
+anprNotWorking = c(
+		#		"Oudebaan 407", # Working
+		  # "Hoegaardsestraat 2", #Working
+		  # "Hoegaardsestraat 2", #Working
+		  # "Martelarenplein", #(geen nummering) working
+		#         Augustijnenstraat (werd niet geplaatst);
+		#         Ferdinand Smoldersplein (geen nummering);
+		#         Lavorenberg (geen nummering);
+		#         Vissersstraat (niet geplaatst);
+				"Bogaardenstraat 27",
+
+				"Bondgenotenlaan 34",
+
+				"Brusselsestraat 24",
+				"Brusselsestraat 21",
+
+				"Collegeberg 20",
+
+				"Diestsestraat 62",
+				"Diestsestraat 48",
+				"Diestsestraat 139",
+				"Diestsestraat 128",
+				"Diestsestraat 261",
+				"Diestsestraat 233",
+				"Diestsestraat 185",
+
+				"Dirk Boutslaan 25",
+
+				"Goudsbloemstraat 1",
+
+				"Herbert Hooverplein 24",
+
+				"Hoegaardsestraat 2",
+
+				"Kapucijnenvoer 8",
+
+				"Krakenstraat 7",
+
+				"Mechelsestraat 50",
+
+				"Monseigneur Ladeuzeplein 17",
+
+				"Monseigneur Ladeuzeplein 24",
+
+				"Naamsestraat 7",
+
+
+				"Parijsstraat 59",
+				"Parijsstraat 72",
+				"Parijsstraat 80",
+
+				"Schrijnmakersstraat 18",
+
+				"’s-Meiersstraat 2",
+
+				"Tiensestraat 62",
+
+				"Vital Decosterstraat 9",
+
+				"Wandelingstraat 11"
+			)
+
+streetsAllowed = c( 
+
+#				"perrons De Lijn",
+
+				"Augustijnenstraat",
+
+				"Boekhandelstraat",
+
+				"Bogaardenstraat",
+
+				"Bondgenotenlaan",
+
+				"Brusselsestraat",
+
+				"Collegeberg",
+
+				"Den Tempst",
+
+				"Diestsestraat",
+
+				"Diestsevest",
+
+				"Dirk Boutslaan",
+
+				"Drie-Engelenberg",
+
+				"Ferdinand Smoldersplein",
+
+				"Goudsbloemstraat",
+
+				"Grote Markt",
+
+				"Herbert Hooverplein",
+
+				"Hoegaardsestraat",
+
+				"Hogeschoolplein",
+
+				"Kapucijnenvoer",
+
+				"Kiekenstraat",
+
+				"Kortestraat",
+
+				"Krakenstraat",
+
+				"Kroegberg",
+
+				"Lavorenberg",
+
+				"Leopold Vanderkelenstraat",
+
+				"Louis Melsensstraat",
+
+				"Margarethaplein",
+
+				"Martelarenplein",
+
+				"Mechelsestraat",
+
+				"Minderbroedersstraat",
+
+				"Monseigneur Ladeuzeplein",
+
+				"Muntstraat",
+
+				"Naamsestraat",
+
+				"Nieuwe Kerkhofdreef",
+
+				"Oostertunnel",
+
+				"Oudebaan",
+
+				"Parijsstraat",
+
+				"Philipssite",
+
+				"Pierre Joseph Van Benedenstraat",
+
+				"Rector De Somerplein",
+
+				"Schrijnmakersstraat",
+
+				"Sint-Barbarastraat",
+
+				"Sint-Maartenstraat",
+
+				"'s-Meiersstraat",
+
+				"Tiensestraat",
+
+				"Tiensevest",
+
+				"Vaartstraat",
+
+				"Vismarkt",
+
+				"Vissersstraat",
+
+				"Vital Decosterstraat",
+
+				"Waaistraat",
+
+				"Wandelingstraat",
+
+				"Zeelstraat"
+			  )
+
+print(anprNotWorking)
+print(streetsAllowed)
+
+addCity <- function(street, details=FALSE){
+  if(details == TRUE){
+    paste(street, ", Leuven, Belgium")
+  }
+}
+
+#streetsAllowedOSM <- streetsAllowed %>% 
+#	map(addCity,details=TRUE) %>%
+#	map(geocode_OSM, details=TRUE)
+
+# Saving due to local cache policy of nominatim.
+#save(streetsAllowedOSM, file="/tmp/wmleuv/streetsAllowedOSM.Rdata")
+#SAVED 						^^^
+
+anprNotWorkingOSM <- anprNotWorking%>% 
+	map(addCity,details=TRUE) %>%
+	map(geocode_OSM, details=TRUE)
+
+# Saving due to local cache policy of nominatim.
+save(anprNotWorkingOSM, file="/tmp/wmleuv/anprNotWorkingOSM.Rdata")
+#SAVED 						^^^
+
+# Single case example first mapping!
+#gbn<-geocode_OSM("Bondgenotenlaan, Leuven, Belgium",details=TRUE)
+#print(gbn)
+#sfdata<-opq_osm_id(id = gbn$osm_id, type = gbn$osm_type)%>%
+#	opq_string() %>%
+#	osmdata_sf()
+#print(sfdata)

d9af7e40c3358bc151c5e2bfd60470d1e3319d13
Dieter Castel <dietercastel@gmail.com>
2019-12-20 19:20:22 +0100

    Highlighted some roads. Not the right ones it seems though! WIP

--- dockerggplot.r
+++ dockerggplot.r
@@ -4,25 +4,51 @@ library(sf)
 library(purrr)
 #library(tmap)
 #library(tmaptools)
-load("/tmp/wmleuv/streets.Rdata")
-load("/tmp/wmleuv/smallstreets.Rdata")
+load("/tmp/wmleuv/streets.Rdata") # streets
+load("/tmp/wmleuv/smallstreets.Rdata") # smallStreets
 load("/tmp/wmleuv/dijle.Rdata")
 load("/tmp/wmleuv/streetsAllowedOSM.Rdata")
 load("/tmp/wmleuv/anprNotWorkingOSM.Rdata")
 load("/tmp/wmleuv/sfAStreets.Rdata")
+load("/tmp/wmleuv/aStreetIds.Rdata") # aStreetIds
 #hhstr<-sf::st_read("/tmp/wmleuv/hhstr.osm", layer = 'points')
 leuvCoord <-getbb("Leuven Belgium")
-print(leuvCoord)
-#print(streets)
+#			x		y
+#min <- c(4.640295,50.824210) 
+#max <- c(4.77053,50.94407) 
+#leuvCoord = data.frame(min,max) 
+#rownames(leuvCoord) <- c("x","y")
+#print(leuvCoord)
+#save(leuvCoord, file="/tmp/wmleuv/leuvCoord.Rdata")
+load("/tmp/wmleuv/leuvCoord.Rdata")
+print(streets)
+print(smallStreets$osm_lines["osm_id"])
+print(streets$osm_lines["osm_id"])
 
 #gbn<-geocode_OSM("Bondgenotenlaan 34, Leuven, Belgium")
 #print(gbn)
+
+aStreetIds <- unlist(aStreetIds)
+
+print(aStreetIds)
+
+
+#print(streets)
+#print(row.names(streets$osm_lines))
+#hlStreets1 <- streets$osm_lines[aStreetIds, ]
+hlStreets1 <- streets$osm_lines [which (streets$osm_lines$osm_id  %in% aStreetIds), ]
+hlStreets2 <- smallStreets$osm_lines [which (streets$osm_lines$osm_id  %in% aStreetIds), ]
+
+print(hlStreets1)
+print(hlStreets2)
 
 xoffset = c(-0.02, 0.03)
 yoffset = c(0.01, 0.00)
 
 xbounds = leuvCoord[1,1:2] + xoffset
 ybounds = leuvCoord[2,1:2] + yoffset
+
+print("test")
 
 # Only three working ones:
 # - Geldkoe-Martenlarenplein 50.88057, 4.71487
@@ -31,11 +57,11 @@ ybounds = leuvCoord[2,1:2] + yoffset
 workingANPR <- data.frame(longitude = c(4.71487,4.722256,4.725876), latitude = c(50.88055,50.868489,50.868851))
 shapesworkingANPR <- c(24,24,25)
 sfworkingANPR <- st_as_sf(workingANPR, coords = c("longitude", "latitude"), crs = 4326, agr = "constant")
-print(sfworkingANPR)
+#print(sfworkingANPR)
 
 print(xbounds)
 print(ybounds)
-print(sfAStreets)
+#print(sfAStreets)
 
 getLines <- function(osmdataObj){
 	osmdataObj$osm_lines
@@ -44,9 +70,14 @@ getLines <- function(osmdataObj){
 sfALines <- sfAStreets%>%
 	map(getLines)
 
-#sfAlinesDf <- data.frame(matrix(unlist(sfALines), nrow=length(sfALines), byrow=T))
+toSFDF <- function(collist){
+	result <- collist%>%
+		map(st_sfc)%>%
+		rbind()
+}
+#SFDFALines <- toSFDF(sfALines)
+#print(SFDFALines)
 
-#print(sfALines)
 
 ggplot() +
   geom_sf(data = streets$osm_lines,
@@ -73,10 +104,15 @@ ggplot() +
 		  size = 4, 
 		  shape = shapesworkingANPR,
 		  fill = "darkred") +
-  #geom_sf(data = sfAlinesDf,
-#		  size = 4, 
-#		  shape = shapesworkingANPR,
-#		  fill = "red") +
+  geom_sf(data = hlStreets1,
+          color = "red",
+          size = .4,
+          alpha = .8) +
+  geom_sf(data = hlStreets2,
+		  inherit.aes = FALSE,
+		  color = "red",
+		  size = .4,
+		  alpha = .6)+
   coord_sf(xlim = xbounds, 
            ylim = ybounds,
            expand = FALSE)+

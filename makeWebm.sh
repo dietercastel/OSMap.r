23c3ff054f1868a24ef2447cd9f6dd6fb4992e84
Dieter Castel <dietercastel@gmail.com>
2020-01-30 15:38:04 +0100

    cleaning up webm making.

--- makeWebm.sh
+++ makeWebm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #		1 frame/s      INPUT frames          encoder, yuva420 for transparancy support ,max twitter scale
-ffmpeg -framerate 1 -i frames/frame%03d.png -c:v libvpx-vp9 -pix_fmt yuva420p -vf scale=720:-1 -lossless 1 output/creepFast.webm
-ffmpeg -framerate 0.4 -i frames/frame%03d.png -c:v libvpx-vp9 -pix_fmt yuva420p -vf scale=720:-1 -lossless 1 output/creepSlow.webm
+ffmpeg -framerate 1 -i frames/frame%1d.png -c:v libvpx-vp9 -pix_fmt yuva420p -vf scale=720:-1 -lossless 1 output/creepFast.webm
+ffmpeg -framerate 0.4 -i frames/frame%1d.png -c:v libvpx-vp9 -pix_fmt yuva420p -vf scale=720:-1 -lossless 1 output/creepSlow.webm
 ffmpeg -framerate 1 -i frames/smallFrame%1d.png -c:v libvpx-vp9 -pix_fmt yuva420p -vf scale=720:-1 -lossless 1 output/creepSmallFast.webm
 ffmpeg -framerate 0.4 -i frames/smallFrame%1d.png -c:v libvpx-vp9 -pix_fmt yuva420p -vf scale=720:-1 -lossless 1 output/creepSmallSlow.webm

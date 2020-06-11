9d183d9a3f88385454db3e01dceb7962202bb7e2
Dieter Castel <dietercastel@gmail.com>
2020-06-11 11:46:06 +0200

    Added tag to docker container pull.

--- dockerPullContainers.sh
+++ dockerPullContainers.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-#docker pull robinlovelace/geocompr
+docker pull robinlovelace/geocompr@sha256:414dd79406e04604c885114e9e0f01ea1da06081606425575f4007b2d2a35237
 docker pull v4tech/imagemagick
 docker pull jrottenberg/ffmpeg

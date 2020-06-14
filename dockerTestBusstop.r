657285ff23dfc615b02b069830fe8517e14b67b2
Dieter Castel <dietercastel@gmail.com>
2020-06-14 21:56:40 +0200

    Some more cleanup.

--- dockerTestBusstop.r
+++
@@ -1,8 +0,0 @@
-library(tidyverse)
-library(osmdata)
-library(sf)
-load("/tmp/wmleuv/rdata/allstreets.Rdata") # allStreets
-print(allStreets$osm_lines)
-# bus bus.lanes bus.oneway bus_bay busway.left busway.right
-busLocs <- allStreets$osm_polygons [which (allStreets$osm_polygons$highway== "platform"), ]
-print(busLocs)
